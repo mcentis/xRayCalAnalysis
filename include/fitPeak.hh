17105b17c3415046191b00eaba983c9537c6253f
mcentis <matteo.centis.vignali@desy.de>
2014-07-18 11:03:02 +0200

    added variables to steer fit range

--- include/fitPeak.hh
+++ include/fitPeak.hh
@@ -1,6 +1,7 @@
 /**
  * function to fit the peak of a histo
  * two fits are performed, a first to find the fit range, and a second to estimate the parameters
+ * posSig and negSig steer the range
  */
 
 #ifndef FITPEAK_HH
@@ -11,9 +12,14 @@
 #include "TAxis.h"
 #include "TCanvas.h"
 
-TF1* fitPeak(TH1* hist)
+TF1* fitPeak(TH1* hist, double negSig = 1, double posSig = 1)
 {
-  if(hist == 0) return 0;
+  if(hist == 0)
+    {
+      TF1* ret = new TF1("ret", "gaus");
+      ret->SetParameters(0, 0, 0);
+      return ret;
+    }
 
   int maxBin = hist->GetMaximumBin();
   double maxPos = hist->GetXaxis()->GetBinCenter(maxBin);
@@ -29,8 +35,8 @@ TF1* fitPeak(TH1* hist)
   fitFunc->SetRange(start, stop);
   hist->Fit(fitFunc, "RQ");
 
-  start = fitFunc->GetParameter(1) - fitFunc->GetParameter(2);
-  stop = fitFunc->GetParameter(1) + fitFunc->GetParameter(2);
+  start = fitFunc->GetParameter(1) - negSig * fitFunc->GetParameter(2);
+  stop = fitFunc->GetParameter(1) + posSig * fitFunc->GetParameter(2);
   fitFunc->SetRange(start, stop);
   hist->Fit(fitFunc, "RQ");
 
