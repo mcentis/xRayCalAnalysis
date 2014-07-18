fd5a3aa545d6469f4a5ab777e9da0573ef84dece
mcentis <matteo.centis.vignali@desy.de>
2014-07-18 14:57:28 +0200

    splitted the function in declaration and implementation to avoid problems in the linking

--- include/fitPeak.hh
+++ include/fitPeak.hh
@@ -9,36 +9,8 @@
 
 #include "TH1.h"
 #include "TF1.h"
-#include "TAxis.h"
-#include "TCanvas.h"
 
-TF1* fitPeak(TH1* hist, double negSig = 1, double posSig = 1)
-{
-  if(hist == 0) return 0;
-
-  int maxBin = hist->GetMaximumBin();
-  double maxPos = hist->GetXaxis()->GetBinCenter(maxBin);
-
-  TCanvas* fitCan = new TCanvas("fitCan");
-
-  TF1* fitFunc = new TF1("fitFunc", "gaus");
-  fitFunc->SetParameter(0, hist->GetMaximum());
-  fitFunc->SetParameter(1, maxPos);
-  fitFunc->SetParameter(2, hist->GetRMS());
-  double start = maxPos - hist->GetRMS();
-  double stop = maxPos + hist->GetRMS();
-  fitFunc->SetRange(start, stop);
-  hist->Fit(fitFunc, "RQ");
-
-  start = fitFunc->GetParameter(1) - negSig * fitFunc->GetParameter(2);
-  stop = fitFunc->GetParameter(1) + posSig * fitFunc->GetParameter(2);
-  fitFunc->SetRange(start, stop);
-  hist->Fit(fitFunc, "RQ");
-
-  delete fitCan;
-
-  return fitFunc;
-}
+TF1* fitPeak(TH1* hist, double negSig = 1, double posSig = 1);
 
 #endif // #ifndef FITPEAK_HH
 
